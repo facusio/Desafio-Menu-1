86345271610178ae2636cc94e9d94aec32ea1e8d
unknown <candelamangino>
2024-08-22 12:25:52 -0300

    Waiter

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -7,5 +7,30 @@ namespace Ucu.Poo.Restaurant;
 /// </summary>
 public class Waiter
 {
+    private string name;
+    public string Name
+    {
+        get
+        {
+            return name;
+            
+        }
+        set
+        {
+            name = value;
+        }
+
+    }
+
+    public void AssignTable(Table table)
+    {
+        this.assignedTables.Add(table);
+    }
+
+    public void TakeOrder(Table table, Dish dish)
+    {
+        table.AddToOrder(dish);
+    }
+        
     private ArrayList assignedTables = new ArrayList();
 }
